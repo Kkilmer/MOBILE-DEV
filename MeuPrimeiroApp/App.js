a82e54305836cfb0aba331bb3f458f7aac2a5b2d
Kkilmer <kp827767@gmail.com>
2025-09-04 20:54:01 -0300

    styles - flex

--- MeuPrimeiroApp/App.js
+++ MeuPrimeiroApp/App.js
@@ -1,12 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import Exercicio02 from './src/components/Exercicio02'
+import Exercicio01 from './src/components/Exercicio01'
 
 const App = () => {
   return (
-    <View>
-      <Exercicio02 />
-    </View>
+    <Exercicio01 name="Kevin Kilmer"/>
   )
 }
 
